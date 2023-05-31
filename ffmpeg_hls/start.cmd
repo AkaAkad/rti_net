d7095f08310853c4f193d5a787528cb6c087b805
AkaAkad <akuahaadji24@outlook.com>
2023-05-31 03:06:47 +0200

    real time infrasturcture and rtmp server

---
+++ ffmpeg_hls/start.cmd
@@ -0,0 +1,1 @@
+ffmpeg -v verbose -re -i 1.mp4 -c:v libx264 -b:v 5000k -f hls -hls_time 6 -hls_list_size 4 -hls_wrap 40 -hls_delete_threshold 1 -hls_flags delete_segments -hls_start_number_source datetime -preset superfast -start_number 10 ./stream.m3u8
